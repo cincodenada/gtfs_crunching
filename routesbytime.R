bed15e6660a3a132177f398a091813252de67ff0
Joel Bradshaw <cincodenada@gmail.com>
2016-02-13 11:29:12 -0800

    Merge in route/trip data, and skip aggregation for now

--- routesbytime.R
+++ routesbytime.R
@@ -1,6 +1,8 @@
 library(ggplot2)
 
-stop_times = read.csv('Trimet_2016-02-13/stop_times.txt',nrows=5000,stringsAsFactors=F)
+stop_times = read.csv('Trimet_2016-02-13/stop_times.txt',stringsAsFactors=F)
+routes = read.csv('Trimet_2016-02-13/routes.txt',stringsAsFactors=F)
+trips = read.csv('Trimet_2016-02-13/trips.txt',stringsAsFactors=F)
 time_to_secs = function(x) {
     parts = unlist(strsplit(x, ':'))
     val = as.numeric(parts[1])*60*60+as.numeric(parts[2])*60+as.numeric(parts[3])
@@ -8,5 +10,10 @@ time_to_secs = function(x) {
 stop_times$arrival_secs = sapply(stop_times$arrival_time, time_to_secs)
 stop_times$departure_secs = sapply(stop_times$departure_time, time_to_secs)
 
-start_end = aggregate(.~trip_id, subset(stop_times, select=c(trip_id,arrival_secs,departure_secs)), function(x) c(minval = min(x), maxval = max(x)))
-start_end
+#start_end = aggregate(.~trip_id, subset(stop_times, select=c(trip_id,arrival_secs,departure_secs)), function(x) c(minval = min(x), maxval = max(x)))
+#alldata = merge(start_end, merge(routes,trips))
+alldata = merge(stop_times, merge(routes,trips))
+colnames(alldata)
+png('test.png',w=1000,h=500)
+ggplot(alldata, aes(x=arrival_secs,group=agency_id,fill=agency_id)) + geom_bar()
+dev.off()
