f0ccd2d3e674c0fb0ee573bc4556fb92429bf295
Joel Bradshaw <cincodenada@gmail.com>
2016-02-13 11:29:12 -0800

    Merge in agency data, and make nice names

--- routesbytime.R
+++ routesbytime.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 
 stop_times = read.csv('Trimet_2016-02-13/stop_times.txt',stringsAsFactors=F)
+agencies = read.csv('Trimet_2016-02-13/agency.txt',stringsAsFactors=F)
 routes = read.csv('Trimet_2016-02-13/routes.txt',stringsAsFactors=F)
 trips = read.csv('Trimet_2016-02-13/trips.txt',stringsAsFactors=F)
 time_to_secs = function(x) {
@@ -12,8 +13,17 @@ stop_times$departure_secs = sapply(stop_times$departure_time, time_to_secs)
 
 #start_end = aggregate(.~trip_id, subset(stop_times, select=c(trip_id,arrival_secs,departure_secs)), function(x) c(minval = min(x), maxval = max(x)))
 #alldata = merge(start_end, merge(routes,trips))
-alldata = merge(stop_times, merge(routes,trips))
+# Copy id -> name generally
+route_names = merge(routes, agencies)
+route_names$display_name = routes$agency_name
+# Special cases
+# Separate MAX and Bus
+route_names$display_name[route_names$agency_id=="TRIMET"] = "TriMet Bus"
+route_names$display_name[grepl("MAX",route_names$route_long_name)] = "TriMet MAX"
+route_names$display_name[grepl("WES",route_names$route_long_name)] = "WES Commuter Rail"
+
+alldata = merge(stop_times, merge(trips, route_names))
 colnames(alldata)
 png('test.png',w=1000,h=500)
-ggplot(alldata, aes(x=arrival_secs,group=agency_id,fill=agency_id)) + geom_bar()
+ggplot(alldata, aes(x=arrival_secs,group=display_name,fill=display_name)) + geom_bar()
 dev.off()
